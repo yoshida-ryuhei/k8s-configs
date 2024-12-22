6f7e47b1b1bb3c5cbe5ada98fcdf9791479283e7
Ryuhei Yoshida <yoshida.ryuhei0112@gmail.com>
2024-12-22 12:06:06 +0900

    ryeで管理へ

--- install_dep_packages.sh
+++ install_dep_packages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # note: https://github.com/kubernetes-client/python/issues/1333
-pip3 install -Iv kubernetes==11.0.0
-pip3 install -Iv openshift==0.11.2
+rye sync
+#pip3 install -Iv kubernetes==11.0.0
+#pip3 install -Iv openshift==0.11.2
 ansible-galaxy collection install kubernetes.core
 
-pip3 install -Iv jinja2==3.0.3
-pip3 install jinja2-base64-filters==0.1.4
+#pip3 install -Iv jinja2==3.0.3
+#pip3 install jinja2-base64-filters==0.1.4
